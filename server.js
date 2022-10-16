121212214040571c321999c06349c4398f944515
MD REJAUR RAHMAN <you@example.com>
2022-10-16 19:51:10 +0600

    trying to connect live server

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const mongodb = require('mongodb');
+const port = 'https://acc-assignmen-2.onrender.com/api/v1/tour';
 
 const app = require('./app');
 
@@ -8,6 +9,6 @@ mongoose.connect('mongodb://localhost:27017/tours').then(() => {
   console.log(`Database connection successfull`);
 });
 
-app.listen(5000, () => {
+app.listen(port, () => {
   console.log('App is listening on port 5000');
 });
